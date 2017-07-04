b8b8ba21f299b53bda57e084e65ae42b2be6a0c5
Shubham <send2shubham@gmail.com>
2017-07-04 17:48:18 +0530

    Add files via upload

---
+++ src/main/java/springjdbc/RmUser.java
@@ -0,0 +1,48 @@
+package springjdbc;
+
+public class RmUser {
+
+	private Integer userId;
+	private String userName;
+	private String password;
+	private String enabled;
+	
+	public RmUser(String userName, String password, String enabled, int userId) {
+		this.userId = userId;
+		this.userName = userName;
+		this.password = password;
+		this.enabled = enabled;
+	}
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(String enabled) {
+		this.enabled = enabled;
+	}
+
+}
