745fccdb83232b87fd76d2973049136b0f1af700
Shubham <send2shubham@gmail.com>
2017-07-04 17:44:54 +0530

    Update testdatabase.sql

--- src/main/java/springjdbc/testdatabase.sql
+++ src/main/java/springjdbc/testdatabase.sql
@@ -1,32 +1,11 @@
--- MySQL Administrator dump 1.4
---
--- ------------------------------------------------------
--- Server version	5.1.35-community
+ 
+ CREATE TABLE TESTSCHEMA.RMUSER ( 
+ username VARCHAR(50),
+ password VARCHAR(50), 
+ enabled VARCHAR(1), 
+ user_id INTEGER NOT NULL, 
+ CONSTRAINT primary_key PRIMARY KEY (user_id) );
 
--- Create schema apu
-
-connect HR/hradmin;
-
--- Definition of table `users`
-
-
-DROP TABLE IF EXISTS 'user';
-
-CREATE TABLE "HR"."USER"
-(   "USERNAME" VARCHAR2(50) NOT NULL,
-    "PASSWORD" VARCHAR2(50) NOT NULL,
-    "ENABLED" VARCHAR2(1) NOT NULL,
-    "USER_ID" NUMBER CONSTRAINT "USER_ID_NN" NOT NULL,
-    CONSTRAINT "USER_ID_PK" PRIMARY KEY ("USER_ID"));
-    
--- Dumping data for table `users`
-
-
-INSERT INTO 'user' ('username','password','enabled','user_id') VALUES 
- ('apurav','apurav','T',3),
- ('apurav','apurav','F',4),
- ('apurav','apurav','F',6),
- ('apurav','apurav','F',7),
- ('apurav','apurav','F',80),
- ('apurav','apurav','F',90);
-
+insert into TESTSCHEMA.RMUSER (username,password,enabled,user_id) values('shubh', '1234', 'T', 1);
+insert into TESTSCHEMA.RMUSER (username,password,enabled,user_id) values('vijay', '1234', 'F', 2);
+insert into TESTSCHEMA.RMUSER (username,password,enabled,user_id) values('ajay', '1234', 'F', 3);
