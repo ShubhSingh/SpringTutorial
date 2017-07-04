6661b777f95680bc1d8051a57fd240c6218250ae
Shubham <send2shubham@gmail.com>
2017-07-04 17:26:54 +0530

    Update Triangle.java

--- src/main/java/tutorial9/aop/Triangle.java
+++ src/main/java/tutorial9/aop/Triangle.java
@@ -4,10 +4,12 @@ public class Triangle {
 	private String name;
 
 	public String getName() {
+		System.out.println("-------- Triangle's getter called. ---------");
 		return name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
+		System.out.println("-------- Triangle's setter called. ---------");
 	}
 }
